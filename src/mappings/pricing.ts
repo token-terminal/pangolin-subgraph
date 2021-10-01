66661e6e75d2062356bd29516fc33b3df4d20c52
Brandon <brandon@pangolin.exchange>
2021-10-01 18:33:25 -0400

    Increase min threshold
    Applies to LPs with less than 5 providers when calculating tracked volume. Increases threshold from $10 to $1000

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -78,7 +78,7 @@ let WHITELIST: string[] = [
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
-let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('10')
+let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('1000')
 
 // minimum liquidity for price to get tracked
 let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('1')
